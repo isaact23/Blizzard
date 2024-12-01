8b93d0a1e273ef85433ffda29c565b09e2c3e09e
Isaac Thompson <shufflesninja@gmail.com>
2024-12-01 00:19:36 -0500

    Change pieces.h

--- pieces.h
+++ pieces.h
@@ -3,25 +3,33 @@
 
 #include <stdint.h>
 
-// Player numbers
-#define WHITE 1
-#define BLACK 2
+// Piece colors
+#define WHITE 1 << 3
+#define BLACK 2 << 3
 
-// Piece type numbers
+// Piece types
+#define KING 0
+#define QUEEN 1
+#define BISHOP 2
+#define KNIGHT 3
+#define ROOK 4
+#define PAWN 5
+
+// Piece type colors
 #define EMPTY 0
 #define EE 0
-#define WK 1
-#define WQ 2
-#define WB 3
-#define WN 4
-#define WR 5
-#define WP 6
-#define BK 7
-#define BQ 8
-#define BB 9
-#define BN 10
-#define BR 11
-#define BP 12
+#define WK KING   | WHITE
+#define WQ QUEEN  | WHITE
+#define WB BISHOP | WHITE
+#define WN KNIGHT | WHITE
+#define WR ROOK   | WHITE
+#define WP PAWN   | WHITE
+#define BK KING   | BLACK
+#define BQ QUEEN  | BLACK
+#define BB BISHOP | BLACK
+#define BN KNIGHT | BLACK
+#define BR ROOK   | BLACK
+#define BP PAWN   | BLACK
 
 // Determine piece color. Return 0 if white, 1 if black, or 2 if empty.
 uint8_t getPieceColor(uint8_t piece);
